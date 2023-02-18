1b1458d4c8073249a241010d1c8f59773f3c37d3
Skeeb32 <skeeb32@gmail.com>
2023-02-18 02:47:21 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let movieNameRef = document.getElementById("movie-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
+
+//Function to fetch data from API
+
+let getMovie = () => {
+  let movieName = movieNameRef.value;
+  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+
+
+}
